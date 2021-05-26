049aec10f27c3642613860d9e249cf805223e2a2
Gabe Knezek <gabe@knezek.net>
2021-05-25 19:08:41 -0700

    Update with comments on how it works

--- verilog_for_fpga/lights.v
+++ verilog_for_fpga/lights.v
@@ -1,68 +1,100 @@
 module lights(
+    /*
+    There are two clocks, for power savings when running on battery.
+    clk_fast is 14.74 MHz (for easy synching with 115200 serial)
+    clk_slow is 32.768 kHz SiTime MEMS osscilator that is very low power
+    */
     input clk_fast,
     input clk_slow,
 
+    /* Signals to/from USB-UART chip. */
     input rx,
     output tx,
 
+    /* The 12 LEDs, connected through 1k resistors (red/green for each LED)  */
     output [23:0] leds,
 
+    /* 6 general purpose pins, on the top left (opposite the battery) */
     output [5:0] user_io,
 
+    /*
+    These are also general purpose pins, routed to the two other headers on
+    the board. You can use them for whatever, but they also match the pinout of
+    the ESP8266 board and the Adafruit BLE board.
+    */
     input ble_rxi,
     output ble_txo,
 
     input esp_rxi,
     output esp_txo,
 
+    /*
+    VBUS from USB connector, through a 1M - 1M voltage divider. (Yes, this
+    should really be 1/3 2/3.) Anyway, in theory this is 1 when USB is
+    connected, 0 otherwise. It's used to determine which clock to use to drive
+    the design. Also, this means that the serial module only works when
+    connected to USB.
+    */
     input vusb_half,
+    
+    /*
+    Clock enable signal to the 14.74 MHz crystal osscilator module, to save
+    power.
+    */
     output clk_fast_en,
 
-
+    /*
+    SPI interface, connects to Flash memory as well as the AVR MCU. Driven
+    as input for maximum power savings, but you could use this as a high-speed
+    data channel with the MCU if desired, or to store persistent data in the
+    Flash memory.
+    */
     input cs_n,
     input sclk,
     input mosi,
     input miso
-
 );
 
+    // Give default values to unused signals.
+    // (User IO should probably just be input.)
     assign user_io = 6'b000000;
-    assign user_io = 4'b0000;
+
     assign ble_txo = 1;
     assign esp_txo = 1;
+
     wire clk;
 
-    wire nope = 0;
-
     // Synchronize vusb_half to avoid metastability as it comes from outside and
-    // is very likely to be noisy.
+    // is very likely to be noisy. Use clk_slow for this since clk_fast may have
+    // been disabled because there was no USB power. :)
     reg [2:0] vusb_half_synch;
     always @ (posedge clk_slow)
         vusb_half_synch <= {vusb_half_synch[1:0], vusb_half};
     wire vusb_half_i = vusb_half_synch[2];
 
-    // Except the clock because then it never works on battery. Sigh. Oh well.
+    // Then, use that debounced signal to control the mux which chooses between
+    // the two clocks.
     assign clk = vusb_half_i ? clk_fast : clk_slow;
-    //assign clk = vusb_half_i ? clk_fast : nope;
-    assign clk_fast_en = vusb_half ? 1'b1 : 1'b0;
+    assign clk_fast_en = vusb_half ? 1'b1 : 1'b0; /* disable if not used */
+    //assign clk = clk_fast;
+    //assign clk_fast_en = 1;
 
-    /*
-    assign cs_n = 1;
-    assign sclk = 0;
-    assign mosi = 0;
-    */
-    // The incoming 14.7 MHz clock is too fast for changing animation patterns or generating a twinkle effect, so divide it into something slower.
+    // The incoming 14.7 MHz clock is too fast for changing animation patterns
+    // or generating a twinkle effect, so divide it into something slower.
+    // (fast or slow depending on which clock is used)
     wire tick, tick_fast, tick_slow;
-    //assign tick = clk;
 
     tickmaker #(.TICK_WIDTH(20)) ticker_fast_clk (clk, 1'b1, tick_fast);
     tickmaker #(.TICK_WIDTH(13)) ticker_slow_clk (clk, 1'b1, tick_slow);
-    assign tick = vusb_half_i ? tick_fast : tick_slow;
+    assign tick = vusb_half_i ? tick_fast : tick_slow; /* depending on clk */
+    //assign tick = tick_fast; /* depending on clk */
 
-    // LED intensity values to feed into per-LED PWM modules, coming from either animation player, random, or user-defined values.
+    // LED intensity values to feed into per-LED PWM modules, coming from either
+    // animation player, random, or user-defined values.
     reg [24*3-1:0] intensities, intensities_i;
     wire [24*3-1:0] intensities_s, intensities_r;
 
+    // A LED intensity PWM module, one for each LED (both red and green).
     pwm p[23:0](
         .clk({24{clk}}),
         .value(intensities),
@@ -91,11 +123,17 @@ module lights(
     // i - user defined intensities
     // anything else - normal switch back and forth
     // (which also gets triggered at startup; some garbage data is fed in ... fixme)
+    // (this also means there's a possibility that the card gets stuck in one of the above upon startup which isn't great. :/)
     //
 
     wire cmd_go;
     wire [7:0] cmd;
     reg [7:0] cmd_latch;
+
+    // Describe a mux which connects the LED PWM modules to one of three 
+    // intensity signals: either the random module, the animation player, or a
+    // set of registers containing user-specified values received via the serial
+    // port.
 
     always @ (*)
         if (cmd_latch == "r")
@@ -105,6 +143,11 @@ module lights(
         else if (cmd_latch == "i")
             intensities <= intensities_i;
         else
+            // By default, the choice depends on whether the board is connected to USB
+            // power or on battery. If on battery, plays the animation which is a
+            // waterfall pattern encouraging you to wash your hands for 20 seconds. :)
+            // If on USB, twinkles the lights randomly forever as a nice nightlight
+            // effect.
             case(vusb_half_i)
                 0: intensities <= intensities_s;
                 1: intensities <= intensities_r;
@@ -267,6 +310,18 @@ module stored_pattern(
 
 endmodule
 
+/*
+    This describes an array of registers that looks like an SRAM logic-block to
+    FPGA synthesis software. Consequently, this module will actually be thrown
+    out and replaced by the hardware SRAM logic, assuming the registers
+    described here are small enough to fit in the hardware block.
+
+    The nice thing about declaring RAMs in this roundabout way is that
+    simulations just work without any vendor-specific RAM wrapper. And, also,
+    one doesn't have to change the RAM wrapper when going from one vendor to
+    another.
+    
+*/
 module ram (din, addr, write_en, clk, dout);
 
     parameter addr_width = 8;
@@ -290,6 +345,12 @@ module ram (din, addr, write_en, clk, dout);
     end 
 endmodule
 
+/*
+    Generates a single-clock pulse periodicaly, useful for driving go logic and
+    the like.
+    Uses the technique outlined here https://www.fpga4fun.com/Counters1.html to
+    avoid a comparator but instead use the carry chain to get the pulse.
+*/
 module tickmaker(input clk, input tickin, output tickout);
 
     parameter TICK_WIDTH = 21;
@@ -333,6 +394,13 @@ module twinkler(input clk, input go, input step, output reg [2:0] intensity = 0)
     end
 endmodule
 
+/*
+    Generates a PWM signal out as value with a duty cycle specified by signal.
+    Uses the technique from here https://www.fpga4fun.com/PWM_DAC_2.html to
+    generate the PWM via a first-order delta-sigma DAC.
+    (I found it hella confusing that this generates a PWM signal at first; you
+     might want to simulate it to prove it to yourself.)
+*/
 module pwm(clk, value, signal);
     input clk;
     input [2:0] value;
@@ -344,6 +412,12 @@ module pwm(clk, value, signal);
     assign signal = PWM[3];
 endmodule
 
+/*
+    Generate a pseudo-random number using a LFSR (linear-feedback shift register).
+    Check out https://en.wikipedia.org/wiki/Linear-feedback_shift_register for
+    how it works.
+    Uses the example from https://www.fpga4fun.com/Counters3.html
+*/
 module LFSR16_1002D(
   input clk,
   input tick,
